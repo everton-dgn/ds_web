f93f97a21bd9491e8c18a5d00f777734ccc1e68a
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-19 09:53:28 -0300

    feat: add propertied in theme

--- src/ui/theme/typographies.ts
+++ src/ui/theme/typographies.ts
@@ -1,13 +1,14 @@
 export const fonts = {
   family:
     "'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
-  weight: {
+  weights: {
     regular: 400,
     medium: 500,
     extraBold: 800
   },
   sizes: {
     headline: '24px',
+    paragraphHighlight: '14px',
     paragraph: '14px',
     caption: '12px'
   }
