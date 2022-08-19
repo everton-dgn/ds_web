6068cebd57f586d6e57235337c90f3533512e342
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-19 10:48:51 -0300

    feat: add function submit and verify empty fields

--- src/ui/pages/Home/index.tsx
+++ src/ui/pages/Home/index.tsx
@@ -1,8 +1,44 @@
+import { FormEvent, useRef, useState } from 'react'
+import { useSetPageTitle } from 'hooks'
 import * as S from './styles'
 import * as C from 'ui/components'
 import { ReactComponent as Logo } from 'ui/assets/images/logo.svg'
 
 const Home = () => {
+  const [disabledButton, setDisabledButton] = useState(true)
+  useSetPageTitle({ pageTitle: 'Informe seus dados' })
+  const refPhone = useRef<HTMLInputElement>(null)
+  const refCpf = useRef<HTMLInputElement>(null)
+  const refEmail = useRef<HTMLInputElement>(null)
+
+  const phone = (): string | undefined => refPhone.current?.value
+  const cpf = (): string | undefined => refCpf.current?.value
+  const email = (): string | undefined => refEmail.current?.value
+
+  const ChecksIfFieldsAreEmpty = (): boolean => {
+    return !(phone() && cpf() && email())
+  }
+
+  const handleChecksIfFieldsAreEmpty = (): void => {
+    if (!ChecksIfFieldsAreEmpty() && disabledButton) {
+      setDisabledButton(false)
+    }
+    if (ChecksIfFieldsAreEmpty() && !disabledButton) {
+      setDisabledButton(true)
+    }
+  }
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    // eslint-disable-next-line no-console
+    console.clear()
+    // eslint-disable-next-line no-console
+    console.log('Dados do Formulário:')
+    // eslint-disable-next-line no-console
+    console.log({ cpf: 3223232323 })
+  }
+
   return (
     <S.Container>
       <Logo
@@ -13,21 +49,41 @@ const Home = () => {
 
       <C.Typography text="Informe seus dados" type="headline" />
 
-      <S.WrapperInputs>
-        <C.Input
-          placeholder="(xx) 0000-0000"
-          label="Número do celular"
-          error={'Error'}
-        />
-        <C.Input placeholder="000.000.000-00" label="Seu CPF" error={''} />
-        <C.Input
-          placeholder="email@mail.com"
-          label="Seu e-mail"
-          error={'Error'}
-        />
-      </S.WrapperInputs>
+      <S.Form onSubmit={e => handleSubmit(e)}>
+        <S.WrapperInputs>
+          <C.Input
+            placeholder="(xx) 0000-0000"
+            label="Número do celular"
+            error={'Error'}
+            ref={refPhone}
+            onChange={handleChecksIfFieldsAreEmpty}
+          />
+          <C.Input
+            placeholder="000.000.000-00"
+            label="Seu CPF"
+            error={''}
+            ref={refCpf}
+            onChange={handleChecksIfFieldsAreEmpty}
+          />
+          <C.Input
+            placeholder="email@mail.com"
+            label="Seu e-mail"
+            error={'Error'}
+            type="email"
+            ref={refEmail}
+            onChange={handleChecksIfFieldsAreEmpty}
+          />
+        </S.WrapperInputs>
 
-      <C.Button text="Próximo" size="medium" color="blue" />
+        <C.Button
+          text="Próximo"
+          size="medium"
+          color="blue"
+          aria-label="Avançar para próxima etapa no formulário"
+          type="submit"
+          disabled={disabledButton}
+        />
+      </S.Form>
     </S.Container>
   )
 }
