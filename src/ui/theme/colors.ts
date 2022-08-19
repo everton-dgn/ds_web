bbfd00d99bba0c79700c58add1cab6ef6ce49bce
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-19 00:45:16 -0300

    refactor: simplifies object

--- src/ui/theme/colors.ts
+++ src/ui/theme/colors.ts
@@ -1,6 +1,6 @@
 export const colors = {
-  'brand-pure': '#1e5bc6',
-  'brand-dark': '#002c7a',
+  brandPure: '#1e5bc6',
+  brandDark: '#002c7a',
   error: '#ff0000',
   grey: '#d0d0d0',
   black: '#282828',
