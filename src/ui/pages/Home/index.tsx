38f6af2852f914ac30608760b4682c0dbe80867e
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-19 10:11:29 -0300

    feat(Home): add components in page and adjustment layout

--- src/ui/pages/Home/index.tsx
+++ src/ui/pages/Home/index.tsx
@@ -1,15 +1,33 @@
 import * as S from './styles'
 import * as C from 'ui/components'
-import { ReactComponent as IconArrowUp } from 'ui/assets/icons/arrow_up.svg'
+import { ReactComponent as Logo } from 'ui/assets/images/logo.svg'
 
 const Home = () => {
   return (
     <S.Container>
-      <C.TitleSection title="Hello World!" as="h1" />
+      <Logo
+        width={166}
+        height={52}
+        title="Logo Carrefour Soluções Financeiras"
+      />
 
-      <br />
+      <C.Typography text="Informe seus dados" type="headline" />
 
-      <IconArrowUp width={50} height={50} title="Arrow pointing up" />
+      <S.WrapperInputs>
+        <C.Input
+          placeholder="(xx) 0000-0000"
+          label="Número do celular"
+          error={'Error'}
+        />
+        <C.Input placeholder="000.000.000-00" label="Seu CPF" error={''} />
+        <C.Input
+          placeholder="email@mail.com"
+          label="Seu e-mail"
+          error={'Error'}
+        />
+      </S.WrapperInputs>
+
+      <C.Button text="Próximo" size="medium" color="blue" />
     </S.Container>
   )
 }
--- src/ui/pages/Home/styles.ts
+++ src/ui/pages/Home/styles.ts
@@ -3,17 +3,33 @@ import T from 'ui/theme'
 
 export const Container = styled.main`
   display: flex;
+  width: 100%;
+  max-width: ${T.grid.container};
   flex-direction: column;
-  margin: 20px auto;
-  padding: 0;
+  margin: 0 auto;
+  padding: 0 ${T.grid.margin};
   align-items: center;
-
-  ${T.breakpoints.custom(800)} {
-    margin: 120px auto;
-    padding: 0 20px;
-  }
 
   svg {
     fill: ${T.colors.brandPure};
+    margin-top: ${T.spacings.s};
+    margin-bottom: ${T.spacings.xxl};
+  }
+`
+
+export const WrapperInputs = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  column-gap: ${T.grid.gutter};
+  row-gap: 5px;
+  width: 100%;
+  padding: ${T.spacings.l} 0;
+
+  ${T.breakpoints.tablet} {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  ${T.breakpoints.desktop} {
+    grid-template-columns: 1fr 1fr 1fr;
   }
 `
