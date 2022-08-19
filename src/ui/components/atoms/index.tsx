c664e8b47009e06338e6958f9870c41ecd59deb1
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-19 10:08:32 -0300

    refactor: import input

--- src/ui/components/atoms/index.tsx
+++ src/ui/components/atoms/index.tsx
@@ -1,4 +1,3 @@
 export { default as Typography } from './Typography'
 export { default as ErrorBoundary } from './ErrorBoundary'
-export { default as Input } from './Input'
 export { default as Button } from './Button'
