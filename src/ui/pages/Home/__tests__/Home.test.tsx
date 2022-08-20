1135773e6e18b9f7b655ad47e1eaecf09201effb
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-20 02:04:51 -0300

    test: ensure you enable the button when all fields are filled

--- src/ui/pages/Home/__tests__/Home.test.tsx
+++ src/ui/pages/Home/__tests__/Home.test.tsx
@@ -1,17 +1,53 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { renderWithProviders } from 'testHelpers/providers'
 import Home from '..'
 
 describe('[Page] Home', () => {
-  it('should render a header', async () => {
+  afterEach(() => jest.clearAllMocks())
+  const user = userEvent.setup({ delay: null })
+
+  it('should render an svg', () => {
     renderWithProviders(<Home />)
-    const title = screen.getByRole('heading', { name: /hello world!/i })
-    expect(title).toBeInTheDocument()
+    const svg = screen.getByTitle(/Logo Carrefour Soluções Financeiras/i)
+    expect(svg).toBeInTheDocument()
   })
 
-  it('should render an svg', async () => {
+  it('Should start disabled the button "Próximo" and should enable it only when the phone field, CPF field and the email field are filled in', async () => {
     renderWithProviders(<Home />)
-    const svg = screen.getByTitle(/arrow pointing up/i)
-    expect(svg).toBeInTheDocument()
+
+    const btnDisabled = screen.getByRole('button', {
+      name: /Avançar para próxima etapa no formulário/i
+    })
+
+    expect(btnDisabled).toBeDisabled()
+
+    const fieldEmail = screen.getByPlaceholderText('email@mail.com')
+    await user.type(fieldEmail, 'valid_email@email.com')
+
+    expect(
+      screen.getByRole('button', {
+        name: /Avançar para próxima etapa no formulário/i
+      })
+    ).toBeDisabled()
+
+    const fieldPhone = screen.getByPlaceholderText('(XX) 90000-0000')
+    const fieldCpf = screen.getByPlaceholderText('000.000.000-00')
+    await user.type(fieldPhone, '(22) 99110-5583')
+    await user.type(fieldCpf, '387.212.729-21')
+
+    expect(
+      screen.getByRole('button', {
+        name: /Avançar para próxima etapa no formulário/i
+      })
+    ).toBeEnabled()
+
+    await user.clear(fieldCpf)
+
+    expect(
+      screen.getByRole('button', {
+        name: /Avançar para próxima etapa no formulário/i
+      })
+    ).toBeDisabled()
   })
 })
