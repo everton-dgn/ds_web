d884c334a75ba98950d6691beaee1678ef4549d7
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-20 10:14:26 -0300

    test: must ensure correct display of error message

--- src/ui/components/molecules/Input/__tests__/Input.test.tsx
+++ src/ui/components/molecules/Input/__tests__/Input.test.tsx
@@ -1,8 +1,12 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { renderWithProviders } from 'testHelpers/providers'
 import Input from '..'
+import T from 'ui/theme'
 
 describe('[Component] Input', () => {
+  const user = userEvent.setup({ delay: null })
+
   it('should render a snapshot', () => {
     const { container } = renderWithProviders(
       <Input
@@ -14,16 +18,50 @@ describe('[Component] Input', () => {
     expect(container).toMatchSnapshot()
   })
 
-  it('should render the Input', () => {
+  it('should the functions onSetField and handleFormat when change text in input', async () => {
     const onSetField = jest.fn()
     renderWithProviders(
       <Input
-        name="searchRepositoriesInTheGithub"
+        name="example"
         placeholder="Nome de usuário"
         onSetField={onSetField}
       />
     )
-    const wrapper = screen.getByRole('textbox')
-    expect(wrapper).toBeInTheDocument()
+    const fieldText = screen.getByPlaceholderText('Nome de usuário')
+    await user.type(fieldText, 'any_text')
+    expect(onSetField).toHaveBeenCalledTimes(8)
+  })
+
+  it('should render an error message stylize when the error exists', () => {
+    renderWithProviders(
+      <Input
+        name="example"
+        placeholder="Nome de usuário"
+        onSetField={jest.fn()}
+        error="any_error"
+      />
+    )
+    const errorMessages = screen.getByRole('alert')
+    expect(errorMessages).toHaveTextContent('any_error')
+    expect(errorMessages).toHaveStyle({
+      color: T.colors.error,
+      textAlign: 'left',
+      justifyContent: 'left',
+      lineHeight: '16px',
+      fontSize: '12px'
+    })
+  })
+
+  it('should render a label if passed as prop', () => {
+    renderWithProviders(
+      <Input
+        name="example"
+        placeholder="Nome de usuário"
+        onSetField={jest.fn()}
+        label="any_label"
+      />
+    )
+    const label = screen.getByText('any_label')
+    expect(label).toHaveTextContent('label')
   })
 })
