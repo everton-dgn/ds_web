90c4ca48258cd7f1e8d3d4b67dffab56bd7d7596
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-20 11:01:28 -0300

    test: ensure show error message when calling formHomeValidate pass cpf

--- src/Validation/formHomeValidate/__tests__/validation.test.ts
+++ src/Validation/formHomeValidate/__tests__/validation.test.ts
@@ -6,4 +6,10 @@ describe('[Validation] formHomeValidate', () => {
     expect(sut('phone', '(00) 99123-8909')).toEqual(['Número inválido'])
     expect(sut('phone', '(77) 99123-8909')).toEqual([])
   })
+
+  it('should return an array of error message for values false when calling "formHomeValidate" method when send "cpf" as parameter and a CPF number falsy', () => {
+    const sut = (name: string, value: string) => formHomeValidate(name, value)
+    expect(sut('cpf', '000.000.000-00')).toEqual(['CPF inválido'])
+    expect(sut('cpf', '558.250.440-00')).toEqual([])
+  })
 })
