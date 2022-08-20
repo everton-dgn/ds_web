7f648836292bee283285779b8f00a5c168b6ed00
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-20 03:20:18 -0300

    test: add snapshot

--- src/ui/components/atoms/Button/__tests__/Button.test.tsx
+++ src/ui/components/atoms/Button/__tests__/Button.test.tsx
@@ -4,16 +4,23 @@ import { renderWithProviders } from 'testHelpers/providers'
 import Button from '..'
 
 describe('[Component] Button', () => {
-  it('should call a function once on click on the button', async () => {
-    const user = userEvent.setup({ delay: null })
-    const onClick = jest.fn()
+  const user = userEvent.setup({ delay: null })
 
+  it('should renderizar um snapshot', () => {
+    const { container } = renderWithProviders(
+      <Button text="Mais Informações" color="blue" size="medium" />
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should call a function once on click on the button', async () => {
+    const onClick = jest.fn()
     renderWithProviders(
       <Button
         onClick={onClick}
         text="Mais Informações"
         color="blue"
-        size="large"
+        size="medium"
       />
     )
 
@@ -24,43 +31,11 @@ describe('[Component] Button', () => {
     expect(onClick).toHaveBeenCalledTimes(1)
   })
 
-  it('should render a small blue button with text', () => {
-    const { container } = renderWithProviders(
-      <Button text="Mais Informações" color="blue" size="small" />
-    )
-
-    const btn = screen.getByRole('button', { name: /Mais Informações/i })
-    expect(btn).toHaveStyle({
-      height: 'min-content',
-      padding: '8px 12px',
-      fontSize: '1.2rem'
-    })
-
-    expect(btn).toBeEnabled()
-    expect(container).toMatchSnapshot()
-  })
-
-  it('should render a medium blue button with text', () => {
-    renderWithProviders(
-      <Button size="medium" text="Mais Informações" color="blue" />
-    )
-
-    const btn = screen.getByRole('button', { name: /Mais Informações/i })
-
-    expect(btn).toHaveStyle({
-      height: 'min-content',
-      padding: '12px 24px',
-      fontSize: '1.4rem'
-    })
-  })
-
   it('should render a disabled Button', () => {
     renderWithProviders(
-      <Button disabled text="Mais Informações" color="blue" size="large" />
+      <Button disabled text="Mais Informações" color="blue" size="medium" />
     )
-
     const btn = screen.getByRole('button', { name: /Mais Informações/i })
-
     expect(btn).toHaveStyle({
       cursor: 'not-allowed',
       opacity: 0.5
@@ -74,13 +49,11 @@ describe('[Component] Button', () => {
         disabled
         text="Mais Informações"
         color="blue"
-        size="large"
         fullWidth={true}
+        size="medium"
       />
     )
-
     const btn = screen.getByRole('button', { name: /Mais Informações/i })
-
     expect(btn).toHaveStyle({ width: '100%' })
   })
 })
--- src/ui/components/molecules/Input/__tests__/Input.test.tsx
+++ src/ui/components/molecules/Input/__tests__/Input.test.tsx
@@ -3,16 +3,27 @@ import { renderWithProviders } from 'testHelpers/providers'
 import Input from '..'
 
 describe('[Component] Input', () => {
+  it('should render a snapshot', () => {
+    const { container } = renderWithProviders(
+      <Input
+        name="searchRepositoriesInTheGithub"
+        placeholder="Nome de usuário"
+        onSetField={jest.fn()}
+      />
+    )
+    expect(container).toMatchSnapshot()
+  })
+
   it('should render the Input', () => {
+    const onSetField = jest.fn()
     renderWithProviders(
       <Input
         name="searchRepositoriesInTheGithub"
         placeholder="Nome de usuário"
+        onSetField={onSetField}
       />
     )
-
     const wrapper = screen.getByRole('textbox')
-
     expect(wrapper).toBeInTheDocument()
   })
 })
