fc9d4659b501aeb748fd351e8c22d09fc0ccfd93
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-19 22:41:28 -0300

    feat(Input): implements mask in the field

--- src/ui/components/molecules/Input/index.tsx
+++ src/ui/components/molecules/Input/index.tsx
@@ -1,29 +1,45 @@
-import { forwardRef, memo } from 'react'
+import { mask } from 'data/mask'
+import { FormEvent, forwardRef, memo, useCallback } from 'react'
 import * as C from 'ui/components'
 import * as S from './styles'
 import { InputProps } from './types'
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ type = 'text', label, error, ...props }, ref) => (
-    <S.Wrapper>
-      {label && (
-        <S.WrapperLabel>
-          <C.Typography text={label} type="paragraphHighlight" align="left" />
-        </S.WrapperLabel>
-      )}
-      <S.Input ref={ref} {...props} type={type} error={error} />
-      <S.WrapperError>
-        {error && (
-          <C.Typography
-            text={error}
-            type="caption"
-            align="left"
-            color="error"
-          />
+  ({ type = 'text', label, error, maskType, ...props }, ref) => {
+    const handleFormat = useCallback(
+      (e: FormEvent<HTMLInputElement>) => {
+        mask(maskType, e)
+      },
+      [maskType]
+    )
+
+    return (
+      <S.Wrapper>
+        {label && (
+          <S.WrapperLabel>
+            <C.Typography text={label} type="paragraphHighlight" align="left" />
+          </S.WrapperLabel>
         )}
-      </S.WrapperError>
-    </S.Wrapper>
-  )
+        <S.Input
+          ref={ref}
+          {...props}
+          type={type}
+          error={error}
+          onChange={handleFormat}
+        />
+        <S.WrapperError>
+          {error && (
+            <C.Typography
+              text={error}
+              type="caption"
+              align="left"
+              color="error"
+            />
+          )}
+        </S.WrapperError>
+      </S.Wrapper>
+    )
+  }
 )
 
 export default memo(Input)
--- src/ui/components/molecules/Input/types.ts
+++ src/ui/components/molecules/Input/types.ts
@@ -1,6 +1,9 @@
 import { InputHTMLAttributes } from 'react'
 
+export type MaskValueType = 'phone' | 'cpf'
+
 export type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   label?: string
   error?: string
+  maskType?: MaskValueType
 }
