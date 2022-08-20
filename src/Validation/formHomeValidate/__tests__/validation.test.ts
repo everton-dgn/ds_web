ac72e3551cd01724d5712383950c2210cc174066
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-20 11:06:44 -0300

    test: ensure show error message when calling formHomeValidate pass email

--- src/Validation/formHomeValidate/__tests__/validation.test.ts
+++ src/Validation/formHomeValidate/__tests__/validation.test.ts
@@ -12,4 +12,10 @@ describe('[Validation] formHomeValidate', () => {
     expect(sut('cpf', '000.000.000-00')).toEqual(['CPF inválido'])
     expect(sut('cpf', '558.250.440-00')).toEqual([])
   })
+
+  it('should return an array of error message for values false when calling "formHomeValidate" method when send "email" as parameter and a invalid email', () => {
+    const sut = (name: string, value: string) => formHomeValidate(name, value)
+    expect(sut('email', 'invalid_email')).toEqual(['E-mail inválido'])
+    expect(sut('email', 'valid_email@mail.com')).toEqual([])
+  })
 })
