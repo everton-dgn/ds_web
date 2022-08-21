c06d2d4fb9c5572a0c41c4c728ff35a56beb6ea7
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-21 20:04:47 -0300

    refactor: remove code and format imports

--- src/data/masks/index.ts
+++ src/data/masks/index.ts
@@ -1,13 +1,11 @@
-import { maskCpf } from 'data/masks/cpf'
-import { maskPhone } from './phone'
 import { FormEvent } from 'react'
+import { maskCpf, maskPhone } from './typesOfMasks'
 import { MaskValueType } from 'ui/components/molecules/Input/types'
 
 export const mask = (
   maskType: MaskValueType,
   e: FormEvent<HTMLInputElement>
 ): string | undefined => {
-  e.currentTarget.maxLength = 35
   if (maskType === 'cpf') return maskCpf(e)
   if (maskType === 'phone') return maskPhone(e)
 }
