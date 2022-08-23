620ab060b30eb64e8f1d856a721fd3832f6cbbf0
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-23 19:42:28 -0300

    test: ensure the correct display of information in the toast

--- src/ui/pages/Home/__tests__/Home.test.tsx
+++ src/ui/pages/Home/__tests__/Home.test.tsx
@@ -1,9 +1,11 @@
 import { act, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { closeToastAdapter } from 'data/toastAdapter'
 import { renderWithProviders } from 'testHelpers/providers'
 import Home from '..'
 
 describe('[Page] Home', () => {
+  afterEach(() => closeToastAdapter())
   const user = userEvent.setup({ delay: null })
 
   it('should render an svg', () => {
@@ -88,8 +90,51 @@ describe('[Page] Home', () => {
         name: /Avançar para próxima etapa no formulário/i
       })
     )
+
     act(() => {
       expect(screen.getAllByRole('alert')[0]).toBeInTheDocument()
     })
+  })
+
+  it('should show an error message after clicking the "Próximo" button, if there is incorrect data', async () => {
+    renderWithProviders(<Home />)
+
+    const fieldEmail = screen.getByPlaceholderText('email@mail.com')
+    const fieldPhone = screen.getByPlaceholderText('(XX) 90000-0000')
+    const fieldCpf = screen.getByPlaceholderText('000.000.000-00')
+    await user.type(fieldEmail, 'invalid_email')
+    await user.type(fieldPhone, '(00) 1111-1111')
+    await user.type(fieldCpf, '111.111.111-11')
+    await user.click(
+      screen.getByRole('button', {
+        name: /Avançar para próxima etapa no formulário/i
+      })
+    )
+    const toastError = await screen.findByText(
+      'Dados Inválidos! Corrija os erros para continuar.'
+    )
+
+    expect(toastError).toBeInTheDocument()
+  })
+
+  it('should show an success message after clicking the "Próximo" button, if there is correct data', async () => {
+    renderWithProviders(<Home />)
+
+    const fieldEmail = screen.getByPlaceholderText('email@mail.com')
+    const fieldPhone = screen.getByPlaceholderText('(XX) 90000-0000')
+    const fieldCpf = screen.getByPlaceholderText('000.000.000-00')
+    await user.type(fieldEmail, 'valid_email@mail.com')
+    await user.type(fieldPhone, '(38) 99223-0011')
+    await user.type(fieldCpf, '358.136.740-80')
+    await user.click(
+      screen.getByRole('button', {
+        name: /Avançar para próxima etapa no formulário/i
+      })
+    )
+    const toastError = await screen.findByText(
+      'Informações Validadas com Sucesso! Celular: (38) 99223-0011, CPF: 358.136.740-80, E-mail: valid_email@mail.com'
+    )
+
+    expect(toastError).toBeInTheDocument()
   })
 })
